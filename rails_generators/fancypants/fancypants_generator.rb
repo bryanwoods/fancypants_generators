001869142551c5b952cd5073badc61a50d068026
Bryan Woods <bryanwoods4e@gmail.com>
2010-01-15 16:48:59 -0500

    Generator now syncs up sass files and javascript, still need to add images

--- rails_generators/fancypants/fancypants_generator.rb
+++ rails_generators/fancypants/fancypants_generator.rb
@@ -11,6 +11,7 @@ class FancypantsGenerator < Rails::Generator::Base
       m.directory 'app/helpers'
       m.directory 'public/stylesheets/sass'
       m.directory 'public/stylesheets/sass/fancypants'
+      m.directory 'public/stylesheets/sass/fancypants/config'
       m.template "layout.html.haml", "app/views/layouts/#{file_name}.html.haml"
       m.file "stylesheet.sass",  "public/stylesheets/sass/#{file_name}.sass"
       m.file "/sass/_config.sass", "public/stylesheets/sass/fancypants/_config.sass"
@@ -18,6 +19,9 @@ class FancypantsGenerator < Rails::Generator::Base
       m.file "/sass/_elements.sass", "public/stylesheets/sass/fancypants/_elements.sass"
       m.file "/sass/_messages.sass", "public/stylesheets/sass/fancypants/_messages.sass"
       m.file "/sass/_reset.sass", "public/stylesheets/sass/fancypants/_reset.sass"
+      m.file "/sass/config/_colors.sass", "public/stylesheets/sass/fancypants/config/_colors.sass"
+      m.file "/sass/config/_typography.sass", "public/stylesheets/sass/fancypants/config/_typography.sass"
+      m.file "/sass/config/_utilities.sass", "public/stylesheets/sass/fancypants/config/_utilities.sass"
       m.file "javascript/fancypants.js", "public/javascripts/fancypants.js"
       m.file "helper.rb", "app/helpers/layout_helper.rb"
     end
