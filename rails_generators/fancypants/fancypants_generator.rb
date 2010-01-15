95d68f689eadebdb2fb5467df32cd7d5455e5ac1
Bryan Woods <bryanwoods4e@gmail.com>
2010-01-15 16:17:08 -0500

    Adding new sass files and README

--- rails_generators/fancypants/fancypants_generator.rb
+++ rails_generators/fancypants/fancypants_generator.rb
@@ -10,8 +10,14 @@ class FancypantsGenerator < Rails::Generator::Base
       m.directory 'public/stylesheets'
       m.directory 'app/helpers'
       m.directory 'public/stylesheets/sass'
-      m.template "haml/layout.html.haml", "app/views/layouts/#{file_name}.html.haml"
-      m.file "sass/stylesheet.sass",  "public/stylesheets/sass/#{file_name}.sass"
+      m.directory 'public/stylesheets/sass/fancypants'
+      m.template "layout.html.haml", "app/views/layouts/#{file_name}.html.haml"
+      m.file "stylesheet.sass",  "public/stylesheets/sass/#{file_name}.sass"
+      m.file "/sass/_config.sass", "public/stylesheets/sass/fancypants/_config.sass"
+      m.file "/sass/_default.sass", "public/stylesheets/sass/fancypants/_default.sass"
+      m.file "/sass/_elements.sass", "public/stylesheets/sass/fancypants/_elements.sass"
+      m.file "/sass/_messages.sass", "public/stylesheets/sass/fancypants/_messages.sass"
+      m.file "/sass/_reset.sass", "public/stylesheets/sass/fancypants/_reset.sass"
       m.file "javascript/fancypants.js", "public/javascripts/fancypants.js"
       m.file "helper.rb", "app/helpers/layout_helper.rb"
     end
