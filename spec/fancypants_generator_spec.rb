43596bd2acc52faabc15a00ac8fedd6c6f070321
Bryan Woods <bryanwoods4e@gmail.com>
2010-01-15 15:52:48 -0500

    Adding base spec

---
+++ spec/fancypants_generator_spec.rb
@@ -0,0 +1,9 @@
+require File.join(File.dirname(__FILE__), "spec_helper.rb")
+require File.join(File.dirname(__FILE__), "../rails_generators/fancypants/fancypants_generator.rb")
+
+describe FancypantsGenerator do
+  it "should return true" do
+    true
+  end
+
+end
